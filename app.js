24adb328ae8a96d55737292721515e3e89fb1fed
John Sextro <john.sextro@gmail.com>
2015-10-18 11:50:52 -0500

    removed unnecessary code

--- app.js
+++ app.js
@@ -7,27 +7,12 @@ var http = require('http')
   , score = require('./score/service');
 
 var app = appBoot.init();
-
-// Compatible
-
-// Now less files with @import 'whatever.less' will work(https://github.com/senchalabs/connect/pull/174)
-var TWITTER_BOOTSTRAP_PATH = './vendor/twitter/bootstrap/less';
-express.compiler.compilers.less.compile = function(str, fn){
-  try {
-    var less = require('less');var parser = new less.Parser({paths: [TWITTER_BOOTSTRAP_PATH]});
-    parser.parse(str, function(err, root){fn(err, root.toCSS());});
-  } catch (err) {fn(err);}
-}
-
 app.use(bodyParser.json());
 
 // REST Routes for the API
 app.get('/get-score/:id', score.getScore);
-
 app.post('/authorize/get-token', authorization.getToken);
 
-// app.post('/api/createshow', showrest.create);
-// app.put('/api/updateshow/:id', showrest.update);
 
 var server = app.listen(3000, function(){
   console.log("Express server listening on port " + app.address().port, app.settings.env);
