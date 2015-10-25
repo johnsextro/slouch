4579916257dbb15d42573e50e3767e77e7ade529
John Sextro <john.sextro@gmail.com>
2015-10-25 10:19:36 -0500

    reorganized the tests to take better advantage of the suite layout

--- test/vouch-score-suite.js
+++ test/vouch-score-suite.js
@@ -12,16 +12,16 @@ suite.discuss('When using vouch-score')
      .expect('should respond with a score equal to 20', function (err, res, body) {
            assert.isNotNull(body);
            assert.deepEqual(JSON.parse(body), {"score": 20});
-      })
-     .next()
-     .undiscuss()
+      });
+
+suite.undiscuss()
      .discuss('the get-score endpoint should respond with a 400 Bad Request status when the id is not found')
      .get('/get-score/-1/')
-     .expect(400)
-     .next()
-     .undiscuss()
+     .expect(400);
+
+suite.undiscuss()
      .discuss('the get-score endpoint should respond with a 404 Not Found status when the id is not provided')
      .get('/get-score/')
-     .expect(404)
-     .undiscuss().undiscuss()
-     .export(module);
+     .expect(404);
+
+suite.export(module);
