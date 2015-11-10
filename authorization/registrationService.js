8c97c3edc413413949cbe5e6b3e3f8661f8cabbc
John Sextro <John.Sextro@gmail.com>
2015-11-09 20:26:49 -0600

    trying to better handle async

--- authorization/registrationService.js
+++ authorization/registrationService.js
@@ -12,13 +12,7 @@ client.on("connect", function () {
 exports.requestToken = function(req, res) {
 	var apiKey = req.body.key;
 	if(apiKey) {
-		client.exists(apiKey, function(err, reply) {
-			if(reply){
-				res.json({token: apiKey});	
-			} else {
-				res.send(401);	
-			}
-		});
+		_this.tokenExists(apiKey, _this.sendTokenResponse)		
 	} else {
 		res.send(400);
 	}
@@ -33,6 +27,20 @@ exports.requestKey = function(req, res) {
 };
 
 // End Public API
+
+exports.tokenExists = function(apiKey, callback) {
+	client.exists(apiKey, function(err, reply) {
+		callback(reply, apiKey, res);
+	});
+}
+
+exports.sendTokenResponse = function(reply, apiKey, res) {
+	if(reply){
+		res.json({token: apiKey});	
+	} else {
+		res.send(401);	
+	}
+}
 
 exports.doesKeyReqHaveRequiredData = function(body) {
 	var retVal = false;
